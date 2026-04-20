002f78aba556e35a4d8e4ac32e13124e7e945135
MR SMILE <162684003+smiletech12@users.noreply.github.com>
2026-04-21 01:15:04 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,23 @@
+// settings.js
+// credit by Kevin tech 
+
+const settings = {
+  SESSION_ID: "", // enter here your session id
+  ownername: "Ｍｒ Ｓｍｉｌｅ",
+  botname: "ＭＡＹＡ - ＸＭＤ",
+  prefa: ['.', '!'],
+  owner: ["254107065646"]
+};
+
+// Export settings for use in other modules
+module.exports = { settings };
+
+// Watch for changes to this file and reload if updated
+const fs = require('fs');
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log('\x1b[0;32m' + __filename + ' \x1b[1;32mupdated!\x1b[0m');
+  delete require.cache[file];
+  require(file);
+});
